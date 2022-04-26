6b55eaecbc045875bf4c75e683f07173e561a155
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-26 13:38:52 +0530

    errors fixed | no css loading fixed | congif file made

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/*.{html, js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// This file is necessary for the running of the tailwind css.
+// if this file is not made the css rules don't apply to the html file.
+// to create this file we should use the command "npm tailwind init"
+// then add the link to the folder where all the html files are located.
